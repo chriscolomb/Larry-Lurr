8187bbe6304acfe1d648cefb6f96c74011ff6ced
chriscolomb <colomb.code@gmail.com>
2022-08-10 23:15:43 -0700

    Refresh

--- bot.py
+++ bot.py
@@ -245,3 +245,4 @@ async def help(ctx):
 
 bot.run('OTk0NzAyMjIyNDE3OTkzODIw.GM_zi3.YmnpRUQEDp6Et_F0n30e5egRYtVRxAZNoAbXZU') #Real
 # bot.run('OTk5Nzc4NjMwMjg2NzA4ODI2.GwaPvU.mHwZ9zrLTwQ1ZiIByD9Yj5yb2Oj008YhbOXfJ0')  # Fake
+# refresh repo
